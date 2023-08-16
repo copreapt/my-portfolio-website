e09875c51167b43e1acb1f4ef2f188a103f52691
Oprea Catalin <copreapt@gmail.com>
2023-08-16 15:24:59 +0300

    second commit, test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { HomePage } from './pages';
 function App() {
 
 
   return (
     <>
       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
         <ToastContainer position="top-center" />
       </BrowserRouter>
     </>
